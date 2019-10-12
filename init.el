b29affe40e6fa33665df0ab9fc2d0c3a12b5e1ac
dylan socolobsky <dsocolobsky@dc.uba.ar>
2019-10-12 20:19:57 -0300

    initial commit v

--- init.el
+++ init.el
@@ -2,8 +2,9 @@
 
 (set-frame-font "Gohufont Bold 11" nil t)
 
-(setq make-backup-files nil) ; stop creating backup~ files
-(setq auto-save-default nil) ; stop creating #autosave# files
+; Not necessary as per sanemacs.el but I keep this just in case
+;(setq make-backup-files nil) ; stop creating backup~ files
+;(setq auto-save-default nil) ; stop creating #autosave# files
 
 ; Replace text when writing whilst text is selected
 (setq x-select-enable-primary t)
